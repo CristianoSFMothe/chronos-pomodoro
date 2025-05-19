7d9d9c9086cb5dd846586c928d07268caec15cf7
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-19 20:44:16 -0300

    feat: implement task creation functionality in MainForm component

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,28 @@ import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../button';
 import { Cyclos } from '../Cyclos';
 import { DefaultIndex } from '../Input';
+import type React from 'react';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+  const handleCreateNewTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('criar nova tarefa');
+  };
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultIndex
           id='formInput'
           type='text'
           labelText='Tarefa'
+          title='Tarefa'
           placeholder='Digite uma tarefa'
+          value={taskName}
+          onChange={event => setTaskName(event.target.value)}
         />
       </div>
 
