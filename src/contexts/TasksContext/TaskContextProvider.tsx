b78be33b4d03f47c959ab08a497719658e37defe
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-26 18:34:33 -0300

    feat: Add loadBeep function to play gravitational beep audio

--- src/contexts/TasksContext/TaskContextProvider.tsx
+++ src/contexts/TasksContext/TaskContextProvider.tsx
@@ -1,23 +1,29 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkManager } from '../../workers/TimerWorkManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
-export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
+export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkManager.getInstance();
 
-  worker.onmessage(event => {
-    const countDownSeconds = event.data;
+  worker.onmessage(e => {
+    const countDownSeconds = e.data;
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -38,9 +44,17 @@ export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
     worker.postMessage(state);
   }, [worker, state]);
 
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
+
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
     </TaskContext.Provider>
   );
-};
+}
