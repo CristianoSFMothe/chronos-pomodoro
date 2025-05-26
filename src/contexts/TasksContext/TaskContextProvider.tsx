447c644c49edb030597b5c935b510aed3b3ca8fc
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-05-26 17:30:53 -0300

    feat: implement TimerWorkManager for task countdown and update task state on completion

--- src/contexts/TasksContext/TaskContextProvider.tsx
+++ src/contexts/TasksContext/TaskContextProvider.tsx
@@ -2,6 +2,8 @@ import { useEffect, useReducer } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
+import { TimerWorkManager } from '../../workers/TimerWorkManager';
+import { TaskActionTypes } from './taskActions';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,9 +12,31 @@ type TaskContextProviderProps = {
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkManager.getInstance();
+
+  worker.onmessage(event => {
+    const countDownSeconds = event.data;
+
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
+  });
+
   useEffect(() => {
-    console.log('TaskContextProvider state:', state);
-  }, [state]);
+    if (!state.activeTask) {
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
